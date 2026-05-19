62c50dbbd0fd32af507445d8d119faf21d7d4b39
ngmnduc <minhductn0408@gmail.com>
2026-05-19 12:27:40 +0700

    Fix font

--- fe/tailwind.config.ts
+++ fe/tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
